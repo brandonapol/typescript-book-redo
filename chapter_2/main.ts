b28aee0b0e0a402f692b0006dfc4cad38a033fee
Brandon Apol <brandonapol@cedarville.edu>
2022-10-31 22:23:06 -0700

    started chapter two

---
+++ chapter_2/main.ts
@@ -0,0 +1,7 @@
+let ein: number = 1;
+let zwei: string = 'hello';
+let drei: boolean[] = [true, false] // an array of booleans
+// Can also just allow TS to infer types
+
+// 3 + [1]; // JS will allow this! 
+// (3).toString() + [1].toString() // This is what JS thinks you're asking for it; works in TS because we tell it what to do
