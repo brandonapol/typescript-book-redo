ffe25f8eaad20b4624a4b599140f05f9966c91a0
Brandon Apol <brandonapol@cedarville.edu>
2022-10-31 21:54:35 -0700

    initial commit

---
+++ introduction/main.js
@@ -0,0 +1,8 @@
+3 + [] // evaluates to 3
+let obj = {}
+obj.foo // undefined
+
+function a(b) {
+    return b/2;
+}
+a("z") // becomes NaN
---
+++ introduction/main.ts
@@ -0,0 +1,8 @@
+// 3 + [] // evaluates to 3
+// let thing = {}
+// thing.foo // undefined
+
+// function c(d) {
+//     return d/2;
+// }
+// c("z") // becomes NaN
