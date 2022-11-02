f0f15a482105ab42f10030dc2adfd157b36c1b54
Brandon Apol <brandonapol@cedarville.edu>
2022-11-02 00:52:26 -0700

    finished chapter three

--- chapter_3/src/index.ts
+++ chapter_3/src/index.ts
@@ -26,3 +26,77 @@ let a: true = true; // Is a boolean type literal
 
 //* symbol
 // Unique - even if a= Symbol(1) and b = Symbol(1), a ==b will return false
+
+//* Objects
+// Objects should be inferred, or otherwise you have to tell the object the type of every value inside it
+// object is very vague so TS will try to play security - it wants us to be specific!
+// objects are still very much mutable in TS
+
+let aa: {b: number}; // aka 'let aa be an object that contains a number b'
+
+// aa = {};
+aa = {
+    b: 1,
+    // c: 2,
+}
+
+// difference between {}, object, and Object (p.29-30)
+type Age = number 
+type Person = {
+    name: string
+    age: Age 
+}
+
+let age: Age = 55
+let driver: Person = {
+    name: 'James May',
+    age: age
+} // This is beautiful
+
+// can't declare a type twice - 
+type Color = 'red'
+// type Color = 'blue'
+
+
+// UNIONS - re-draw pg. 32
+type Cat = { name: string, purrs: boolean }
+type Dog = { name: string, barks: boolean, wags: boolean }
+type CatOrDogOrBoth = Cat | Dog 
+type CatAndDog = Cat & Dog 
+
+let rex = {
+    name: 'Rex',
+    barks: true,
+    wags: true,
+}
+
+let confused = {
+    name: 'Blex',
+    barks: true,
+    wags: true,
+    purrs: true,
+}
+
+// Out in the wild
+function trueOrNull (isTrue: boolean) {
+    if (isTrue) {
+        return true 
+    }
+    return null
+} // What is the type value of the return of this function? 
+type Returns = string | null 
+
+const something = (a: string, b: number) => {
+    return a || b
+} // same thing; the data returned can be either one; so it's a union
+
+// Try and design programs so arrays have all the same type! TS will only allow 
+// union data types in an array if you explicitly tell it to
+
+let bb = [1, 'a'] // (string | number)
+// readonly arrays - immutable
+
+//* null - absence of value
+//* undefined - variable that hasn't been given a value yet
+//* void - function that doesn't have a return statement
+//* never - function that doesn't return ever
